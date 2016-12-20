6ab38d346a6167136ba1b3a39f748d07ade40939
frostkom <martin.fors@frostkom.se>
2016-12-20 18:00:35 +0100

    Add Plugin URI

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 /*
 Plugin Name: MF Site Manager
-Plugin URI: 
+Plugin URI: https://github.com/frostkom/mf_site_manager
 Description: 
-Version: 4.1.0
+Version: 4.1.1
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_site_manager
 Domain Path: /lang
+
+GitHub Plugin URI: frostkom/mf_site_manager
 */
 
 if(is_admin())
