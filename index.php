a73263aa14589a0feee5666d2c096e8bc2bc54d2
frostkom <martin.fors@frostkom.se>
2021-03-29 09:26:16 +0200

    Make sure that Base is active

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Site Manager
 Plugin URI: https://github.com/frostkom/mf_site_manager
 Description: 
-Version: 5.2.36
+Version: 5.2.37
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,40 +14,43 @@ Depends: MF Base
 GitHub Plugin URI: frostkom/mf_site_manager
 */
 
-include_once("include/classes.php");
-
-$obj_site_manager = new mf_site_manager();
-
-add_action('cron_base', array($obj_site_manager, 'cron_base'), mt_rand(1, 10));
-
-if(is_admin())
+if(is_plugin_active("mf_base/index.php"))
 {
-	load_plugin_textdomain('lang_site_manager', false, dirname(plugin_basename(__FILE__))."/lang/");
+	include_once("include/classes.php");
 
-	register_uninstall_hook(__FILE__, 'uninstall_site_manager');
+	$obj_site_manager = new mf_site_manager();
 
-	add_action('admin_init', array($obj_site_manager, 'admin_init'), 0);
-	add_action('admin_init', array($obj_site_manager, 'settings_site_manager'));
-	add_action('admin_menu', array($obj_site_manager, 'admin_menu'));
+	add_action('cron_base', array($obj_site_manager, 'cron_base'), mt_rand(1, 10));
 
-	if(is_multisite())
+	if(is_admin())
 	{
-		add_action('manage_plugins_columns', array($obj_site_manager, 'manage_plugins_columns'));
-		add_action('manage_plugins_custom_column', array($obj_site_manager, 'manage_plugins_custom_column'), 10, 3);
+		load_plugin_textdomain('lang_site_manager', false, dirname(plugin_basename(__FILE__))."/lang/");
 
-		add_filter('manage_sites-network_columns', array($obj_site_manager, 'sites_column_header'), 5);
-		add_action('manage_sites_custom_column', array($obj_site_manager, 'sites_column_cell'), 5, 2);
-		add_filter('manage_sites_action_links', array($obj_site_manager, 'sites_row_actions'));
+		register_uninstall_hook(__FILE__, 'uninstall_site_manager');
 
-		add_action('admin_footer', array($obj_site_manager, 'admin_footer'), 0);
+		add_action('admin_init', array($obj_site_manager, 'admin_init'), 0);
+		add_action('admin_init', array($obj_site_manager, 'settings_site_manager'));
+		add_action('admin_menu', array($obj_site_manager, 'admin_menu'));
+
+		if(is_multisite())
+		{
+			add_action('manage_plugins_columns', array($obj_site_manager, 'manage_plugins_columns'));
+			add_action('manage_plugins_custom_column', array($obj_site_manager, 'manage_plugins_custom_column'), 10, 3);
+
+			add_filter('manage_sites-network_columns', array($obj_site_manager, 'sites_column_header'), 5);
+			add_action('manage_sites_custom_column', array($obj_site_manager, 'sites_column_cell'), 5, 2);
+			add_filter('manage_sites_action_links', array($obj_site_manager, 'sites_row_actions'));
+
+			add_action('admin_footer', array($obj_site_manager, 'admin_footer'), 0);
+		}
 	}
-}
 
-add_action('wp_ajax_force_server_ip', array($obj_site_manager, 'force_server_ip'));
+	add_action('wp_ajax_force_server_ip', array($obj_site_manager, 'force_server_ip'));
 
-function uninstall_site_manager()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_server_ip', 'setting_server_ips_allowed', 'setting_site_comparison'),
-	));
+	function uninstall_site_manager()
+	{
+		mf_uninstall_plugin(array(
+			'options' => array('setting_server_ip', 'setting_server_ips_allowed', 'setting_site_comparison'),
+		));
+	}
 }
