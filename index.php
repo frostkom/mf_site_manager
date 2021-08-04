65b49c6b734e3c499b1dba7fdf23ac591ca6e425
Martin Fors <martin.fors@frostkom.se>
2021-08-04 03:53:21 +0200

    Make extra check before looking for Base plugin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Site Manager
 Plugin URI: https://github.com/frostkom/mf_site_manager
 Description: 
-Version: 5.3.1
+Version: 5.3.2
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,7 +14,7 @@ Depends: MF Base
 GitHub Plugin URI: frostkom/mf_site_manager
 */
 
-if(is_plugin_active("mf_base/index.php"))
+if(function_exists('is_plugin_active') && is_plugin_active("mf_base/index.php"))
 {
 	include_once("include/classes.php");
 
