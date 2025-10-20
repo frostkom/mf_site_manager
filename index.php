c257323b032282e829e729fbc3baf667f0751150
frostkom <martin.fors@frostkom.se>
2025-10-20 06:13:55 +0200

    Remove unused style

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Site Manager
 Plugin URI: https://github.com/frostkom/mf_site_manager
 Description:
-Version: 5.6.51
+Version: 5.6.52
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
