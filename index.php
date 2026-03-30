558d9361ef7611c0f14b5466de375ca1bf807366
frostkom <martin.fors@frostkom.se>
2026-03-30 11:54:43 +0200

    Make sure that footer output is not overwritten

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Site Manager
 Plugin URI: https://github.com/frostkom/mf_site_manager
 Description: Add MultiSite functionality to compare sites, clone data etc.
-Version: 5.7.1
+Version: 5.7.2
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
