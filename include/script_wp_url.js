e9b1f9cde81a6b1ebb93ab23a793d5acd4a66fe7
frostkom <martin.fors@frostkom.se>
2026-05-06 20:44:19 +0200

    Allow URL fields to be editable

--- include/script_wp_url.js
+++ include/script_wp_url.js
@@ -1,4 +1,4 @@
 jQuery(function($)
 {
-	$("#url, #siteurl, #home").prop('readonly', true).css({'margin-right': '1em', 'width': '50%'}).after("<a href='" + script_site_manager_url.change_url_link + "'>" + script_site_manager_url.change_url_text + "</a>");
+	$("#url, #siteurl, #home")/*.prop('readonly', true)*/.css({'margin-right': '1em', 'width': '50%'}).after("<a href='" + script_site_manager_url.change_url_link + "'>" + script_site_manager_url.change_url_text + "</a>");
 });
